1116c8ce805380e425e9b1167644b5ee0b21fa1d
wenceslauthiagon <wenceslauncthiago@outlook.com>
2023-03-03 12:42:39 -0300

    Including validations in createNotifications.

--- src/create-notification-body.ts
+++ src/create-notification-body.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
 export class CreateNotificationBody {
+  @IsUUID()
+  @IsNotEmpty()
   recipientId: string;
+
+  @IsNotEmpty()
+  @Length(12, 240)
   content: string;
+
+  @IsNotEmpty()
   category: string;
 }
