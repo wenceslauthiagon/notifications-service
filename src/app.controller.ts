8db92f35e672e4d82c468f3f016e169b43c2874a
wenceslauthiagon <wenceslauncthiago@outlook.com>
2023-03-03 11:54:30 -0300

    Adjusting the createNotifications controller.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto';
 
 @Controller('notifications')
 export class AppController {
@@ -8,5 +9,18 @@ export class AppController {
   @Get()
   getNotifications() {
     return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async createNotifications(@Body() body: any) {
+    const { recipientId, content, category } = body;
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        recipientId: recipientId,
+        content: content,
+        category: category,
+      },
+    });
   }
 }
